fcd25cb55351759b31c6cdb39f586e3ebbd1a497
Vamsibadam <vamsibadam2218@gmail.com>
2026-06-15 13:01:16 +0530

    fix static url for production

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ CORS_ALLOWED_ORIGINS = config(
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
